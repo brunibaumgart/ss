f92e50f7de0bc584d20ead036388a5c12524104a
Nicolás Margenat <nmargenat@itba.edu.ar>
2024-08-11 11:28:45 -0300

    feat: add particle creation with list of radius

--- tp1/simulations/src/utils/ParticleUtils.java
+++ tp1/simulations/src/utils/ParticleUtils.java
@@ -28,16 +28,41 @@ public class ParticleUtils {
             double x = random.nextDouble() * L;
             double y = random.nextDouble() * L;
 
-            Particle ip = new Particle(i, new Point(x, y), r);
-            while (particles.contains(ip)) {
+            Particle p = new Particle(i, new Point(x, y), r);
+            while (particles.contains(p)) {
                 x = random.nextDouble() * L;
                 y = random.nextDouble() * L;
-                ip = new Particle(i, new Point(x, y), r);
+                p = new Particle(i, new Point(x, y), r);
             }
 
-            particles.add(ip);
+            particles.add(p);
         }
 
         return particles;
     }
+
+    public static List<Particle> createParticles(final int N, final Double L, List<Double> radius) {
+        if(radius.size() != N)
+            throw new IllegalArgumentException("The number of particles must be the same as the number of radius");
+
+        final List<Particle> particles = new ArrayList<>();
+        final Random random = new Random();
+
+        for (int i = 0; i < radius.size(); i++) {
+            double x = random.nextDouble() * L;
+            double y = random.nextDouble() * L;
+
+            Particle p = new Particle(i, new Point(x, y), radius.get(i));
+            while (particles.contains(p)) {
+                x = random.nextDouble() * L;
+                y = random.nextDouble() * L;
+                p = new Particle(i, new Point(x, y), radius.get(i));
+            }
+
+            particles.add(p);
+        }
+
+        return particles;
+    }
+
 }
