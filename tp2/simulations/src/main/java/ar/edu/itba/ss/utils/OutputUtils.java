3e80fe46857fb99ca3dfefb65e46721802335474
Nicolás Margenat <nmargenat@itba.edu.ar>
2024-08-18 15:13:50 -0300

    fix: correct locale so that dots are used instead of commas in float numbers

--- tp2/simulations/src/main/java/ar/edu/itba/ss/utils/OutputUtils.java
+++ tp2/simulations/src/main/java/ar/edu/itba/ss/utils/OutputUtils.java
@@ -8,24 +8,28 @@ import ar.edu.itba.ss.models.Particle;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class OutputUtils {
+    private final static Locale LOCALE = Locale.US;
+
+
     private OutputUtils() {
         throw new IllegalStateException("Utility class");
     }
 
     public static void printParameters(FileWriter writer, Parameters parameters) {
         try {
-            writer.write(String.format("N %d\n", parameters.getN()));
-            writer.write(String.format("L %.2f\n", parameters.getL()));
-            writer.write(String.format("R %.2f\n", parameters.getR()));
-            writer.write(String.format("V %.2f\n", parameters.getV()));
-            writer.write(String.format("RC %.2f\n", parameters.getRc()));
-            writer.write(String.format("Etha %.2f\n", parameters.getEtha()));
-            writer.write(String.format("M %d\n", parameters.getM()));
-            writer.write(String.format("Iterations %d\n", parameters.getIterations()));
-            writer.write(String.format("Dt %d\n", parameters.getDt()));
+            writer.write(String.format(LOCALE, "N %d\n", parameters.getN()));
+            writer.write(String.format(LOCALE, "L %.2f\n", parameters.getL()));
+            writer.write(String.format(LOCALE, "R %.2f\n", parameters.getR()));
+            writer.write(String.format(LOCALE, "V %.2f\n", parameters.getV()));
+            writer.write(String.format(LOCALE, "RC %.2f\n", parameters.getRc()));
+            writer.write(String.format(LOCALE, "Etha %.2f\n", parameters.getEtha()));
+            writer.write(String.format(LOCALE, "M %d\n", parameters.getM()));
+            writer.write(String.format(LOCALE, "Iterations %d\n", parameters.getIterations()));
+            writer.write(String.format(LOCALE, "Dt %d\n", parameters.getDt()));
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
@@ -43,7 +47,7 @@ public class OutputUtils {
 
     public static void printParticleData(FileWriter writer, MovingParticle particle, List<? extends Particle> neighbours) throws IOException {
         if (neighbours == null || neighbours.isEmpty()) {
-            writer.write(String.format("%s %.2f %.2f %.2f %.2f []\n", particle.id(), particle.position().x(), particle.position().y(), particle.velocity().x(), particle.velocity().y()));
+            writer.write(String.format(LOCALE, "%s %.2f %.2f %.2f %.2f []\n", particle.id(), particle.position().x(), particle.position().y(), particle.velocity().x(), particle.velocity().y()));
             writer.flush();
             return;
         }
@@ -52,7 +56,7 @@ public class OutputUtils {
                 .map(Particle::id)
                 .map(String::valueOf)
                 .collect(Collectors.joining(" "));
-        writer.write(String.format("%s %.2f %.2f %.2f %.2f [%s]\n", particle.id(), particle.position().x(), particle.position().y(), particle.velocity().x(), particle.velocity().y(), neighboursIds));
+        writer.write(String.format(LOCALE, "%s %.2f %.2f %.2f %.2f [%s]\n", particle.id(), particle.position().x(), particle.position().y(), particle.velocity().x(), particle.velocity().y(), neighboursIds));
         writer.flush();
     }
 }
