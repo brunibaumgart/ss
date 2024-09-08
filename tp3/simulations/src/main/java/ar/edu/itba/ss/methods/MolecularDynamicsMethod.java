101a6c46368d8b2515bbb5f712758e41e39065d0
Juan Burda <jburda@itba.edu.ar>
2024-09-07 21:15:27 -0300

    refactor: removed unused methods in molecular dynamics method

--- tp3/simulations/src/main/java/ar/edu/itba/ss/methods/MolecularDynamicsMethod.java
+++ tp3/simulations/src/main/java/ar/edu/itba/ss/methods/MolecularDynamicsMethod.java
@@ -2,12 +2,10 @@ package ar.edu.itba.ss.methods;
 
 import ar.edu.itba.ss.models.BoxState;
 import ar.edu.itba.ss.models.Particle;
-import ar.edu.itba.ss.models.Vector;
 import ar.edu.itba.ss.models.events.CollisionEvent;
 import ar.edu.itba.ss.utils.CollisionUtils;
 import ar.edu.itba.ss.utils.ParticleUtils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 
@@ -57,62 +55,5 @@ public class MolecularDynamicsMethod {
         boxState.incrementIteration();
 
         return boxState;
-    }
-
-    private static List<Particle> calculateCollisionParticles(List<Particle> particles, Particle particle) {
-        final List<Particle> collisionParticles = new ArrayList<>();
-        final double angle = particle.position().angle();
-        final double x = particle.position().x();
-        final double y = particle.position().y();
-
-        // TODO: check border cases (angle = 90, for example)
-        if (angle < 90) {
-            for (Particle potentialParticle : particles) {
-                if (potentialParticle.position().x() > x && potentialParticle.position().y() > y) {
-                    collisionParticles.add(potentialParticle);
-                }
-            }
-        } else if (angle < 180) {
-            for (Particle potentialParticle : particles) {
-                if (potentialParticle.position().x() < x && potentialParticle.position().y() > y) {
-                    collisionParticles.add(potentialParticle);
-                }
-            }
-        } else if (angle < 270) {
-            for (Particle potentialParticle : particles) {
-                if (potentialParticle.position().x() < x && potentialParticle.position().y() < y) {
-                    collisionParticles.add(potentialParticle);
-                }
-            }
-        } else {
-            for (Particle potentialParticle : particles) {
-                if (potentialParticle.position().x() > x && potentialParticle.position().y() < y) {
-                    collisionParticles.add(potentialParticle);
-                }
-            }
-        }
-        return collisionParticles;
-    }
-
-    private static double calculateCollisionTime(Particle particle, Particle otherParticle) {
-        final double sigma = particle.radius() + otherParticle.radius();
-        final Vector deltaR = otherParticle.position().subtract(particle.position());
-        final Vector deltaV = otherParticle.speed().subtract(particle.speed());
-
-        final double deltaRDotDeltaR = deltaR.dot(deltaR);
-        final double deltaVDotDeltaV = deltaV.dot(deltaV);
-        final double deltaVDotDeltaR = deltaV.dot(deltaR);
-
-        if (deltaVDotDeltaR >= 0) {
-            return Double.POSITIVE_INFINITY; // TODO: check this
-        }
-
-        final double d = Math.pow(deltaVDotDeltaR, 2) - (deltaVDotDeltaV) * (deltaRDotDeltaR - Math.pow(sigma, 2));
-
-        if (d < 0) {
-            return Double.POSITIVE_INFINITY; // TODO: check this
-        }
-
-        return -((deltaVDotDeltaR + Math.sqrt(d)) / deltaVDotDeltaV);
     }
 }
