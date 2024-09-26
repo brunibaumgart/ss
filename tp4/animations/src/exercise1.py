0dd228d289efc0bf6700c837d3e988015004efa5
Nicolás Margenat <nmargenat@itba.edu.ar>
2024-09-26 19:29:51 -0300

    feat: comparison plot bw verlet and beeman

--- tp4/animations/src/exercise1.py
+++ tp4/animations/src/exercise1.py
@@ -1,21 +1,60 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 from src.constants import FilePaths
 
-# Inicializamos listas para almacenar tiempo y posición
-times = []
-positions = []
 
-# Leemos el archivo y extraemos los datos
-with open(FilePaths.SIMULATIONS_DIR + 'ej1.txt', 'r') as file:
-    for line in file:
-        time, position = map(float, line.split())
-        times.append(time)
-        positions.append(position)
+# Function to read data from file
+def read_data(file_name):
+    times = []
+    positions = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            time, position = map(float, line.split())
+            times.append(time)
+            positions.append(position)
+    return times, positions
 
-# Graficamos posición vs tiempo
-plt.plot(times, positions)
+
+# Function for the analytical solution
+def analytical_solution(t, A, gamma, m, k):
+    term1 = A * np.exp(-gamma / (2 * m) * t)
+    term2 = np.cos(np.sqrt(k / m - (gamma ** 2 / (4 * m ** 2))) * t)
+    return term1 * term2
+
+
+# Read the data for the Beeman and Verlet algorithms
+times_beeman, positions_beeman = read_data(FilePaths.SIMULATIONS_DIR + 'ej1/beeman_0.010000.txt')
+times_verlet, positions_verlet = read_data(FilePaths.SIMULATIONS_DIR + 'ej1/verlet_0.010000.txt')
+
+# Time range for the analytical solution
+t_values = np.linspace(0, max(times_beeman), 500)
+
+# Placeholder values for analytical solution parameters (replace as needed)
+A = 1.0  # Amplitude
+gamma = 100.0  # Damping coefficient
+m = 70.0  # Mass
+k = 10000.0  # Spring constant
+
+# Compute the analytical solution for the given time values
+r_values = analytical_solution(t_values, A, gamma, m, k)
+
+# Plot the Beeman curve
+plt.plot(times_beeman, positions_beeman, label='Beeman')
+
+# Plot the Verlet curve
+plt.plot(times_verlet, positions_verlet, label='Verlet')
+
+# Plot the Analytical Solution curve
+plt.plot(t_values, r_values, label='Analytical Solution', linestyle='--')
+
+# Add labels, title, and legend
 plt.xlabel('Time')
 plt.ylabel('Position')
+
+# Add grid and legend
 plt.grid(True)
+plt.legend()
+
+# Display the plot
 plt.show()
--- tp4/simulations/src/main/java/ar/edu/itba/ss/Exercise1.java
+++ tp4/simulations/src/main/java/ar/edu/itba/ss/Exercise1.java
@@ -12,6 +12,7 @@ import ar.edu.itba.ss.utils.OutputUtils;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class Exercise1 {
     public static void main(String[] args) throws IOException {
@@ -45,7 +46,10 @@ public class Exercise1 {
         final SimulationState state = new SimulationState(particles, systemParameters);
 
         // Create output file
-        final FileWriter writer = new FileWriter(FilePaths.OUTPUT_DIR + "ej1.txt");
+        // CAMBIAR A MANO
+        final String algorithm = "verlet";
+        final String outputFile = String.format(Locale.US, FilePaths.OUTPUT_DIR + "ej1/%s_%f.txt", algorithm, deltaT);
+        final FileWriter writer = new FileWriter(outputFile);
 
         // Iterate over time
         while(state.timeElapsed() < totalTime) {
