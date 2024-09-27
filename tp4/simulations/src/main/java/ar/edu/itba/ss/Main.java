39049c72a4da4b2e7be059ba133deab0b5adeb5c
brunibaumgart <bbaumgart@itba.edu.ar>
2024-09-27 12:00:16 -0300

    feat: Main implementation

--- tp4/simulations/src/main/java/ar/edu/itba/ss/Main.java
+++ tp4/simulations/src/main/java/ar/edu/itba/ss/Main.java
@@ -1,11 +1,37 @@
 package ar.edu.itba.ss;
 
+import ar.edu.itba.ss.constants.FilePaths;
+import ar.edu.itba.ss.models.parameters.Parameters;
+import ar.edu.itba.ss.utils.ArgumentHandlerUtils;
+import ar.edu.itba.ss.utils.OutputUtils;
+
+import java.io.FileWriter;
+import java.io.IOException;
 /**
  * Hello world!
  *
  */
 public class Main {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    private static final String CONFIG_FILE = "config.json";
+
+    public static void main(String[] args) throws IOException {
+        // Get parameters from example.json
+        final Parameters parameters = ArgumentHandlerUtils.getParameters(CONFIG_FILE);
+        final FileWriter parametersWriter = new FileWriter(FilePaths.PARAMETERS_FILE);
+
+        if (parameters.getPlots().getSystemOne().isEnabled()) {
+            Exercise1.run(parameters);
+        }
+        else {
+            if (parameters.getPlots().getFrecuencyVsK().isEnabled()){
+                //FrecuencyVsK.run(parameters);
+            }
+            if (parameters.getPlots().getFrecuencyVsK().isEnabled()){
+                //FrecuencyVsK.run(parameters);
+            }
+            if (parameters.getPlots().getW0VsK().isEnabled()){
+                //W0VsK.run(parameters);
+            }
+        }
     }
 }
