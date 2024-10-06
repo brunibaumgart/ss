2fd4842cd0eaa053b7c5cb5e4fbc0ac2bfade3e9
Juan Burda <jburda@itba.edu.ar>
2024-10-06 18:26:06 -0300

    fix: removed lines and added vertical asymptote

--- tp4/animations/src/amplitude_vs_w.py
+++ tp4/animations/src/amplitude_vs_w.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from src.constants import FilePaths
 
 filename = FilePaths.SIMULATIONS_DIR + 'amplitude_vs_w.txt'
+optimum_w = 49.67294 # define optimum omega for the vertical line
 
 amplitudes = []
 ws = []
@@ -14,13 +15,14 @@ with open(filename, 'r') as file:
         amplitudes.append(a)
         ws.append(w)
 
-# Increase font size globally
-plt.rcParams.update({'font.size': 14})  # Adjust 14 to your preferred font size
+plt.rcParams.update({'font.size': 14})
 
-# Plot with points for each value
-plt.plot(ws, amplitudes, '-o')  # '-o' adds points and connects them with a line
-plt.xlabel(r"$\omega$ (rad/s)", fontsize=16)  # Adjust fontsize if needed
-plt.ylabel('Amplitud (m)', fontsize=16)   # Adjust fontsize if needed
+plt.plot(ws, amplitudes, 'o', markersize=3)
+
+plt.axvline(x=optimum_w, color='red', linestyle='--')
+
+plt.xlabel(r"$\omega$ (rad/s)", fontsize=16)
+plt.ylabel('Amplitud (m)', fontsize=16)
 plt.grid(True)
 plt.tight_layout()
 plt.show()
